f8e479226cc7722363ee999fd378bdbe64debf7d
Claenio <claenio.souza@gmail.com>
2026-03-14 20:21:34 -0300

    started cash exercise

--- CS50-exercises/cash/src/Main.java
+++ CS50-exercises/cash/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Cash money = new Cash();
 
-            System.out.print("Set a value: ");
+            System.out.print("Set a value(1 for $0.01, 10 for $0,10 100 for $1.00...: ");
             money.setCash(scan.nextInt());
             while (money.getCash() <= 0){
                 System.out.print("Enter an integer: ");

7e01c4d72d31a5be16adc3f0c6e28fa155e8e3cc
Claenio <claenio.souza@gmail.com>
2026-03-17 22:04:39 -0300

    card verification added

--- CS50-exercises/credit/src/Credit.java
+++ CS50-exercises/credit/src/Credit.java
@@ -1,8 +1,29 @@
 public class Credit {
     long cardNumber = 4003600000000014L;
-    long tempCardN;
+    long tempCard = cardNumber;
+    int sumN;
+    int sumMult;
+    int position = 1;
 
-    int sum = 0;
-    int sumR = 0;
-    int counterPosition = 1;
+    public void cardValidation(){
+        while (tempCard > 0){
+            int lastDigit = (int) (tempCard % 10);
+
+            if (position % 2 == 1) {
+                sumN += lastDigit;
+            } else {
+                int multiplied = lastDigit * 2;
+                sumMult += (multiplied % 10) + (multiplied / 10);
+            }
+            tempCard /= 10;
+            position++;
+
+        }
+        int totalSum = sumN + sumMult;
+        if (totalSum % 10 == 0){
+            System.out.println("Valid credit card");
+        } else {
+            System.out.println("Invalid credit card");
+        }
+    }
 }

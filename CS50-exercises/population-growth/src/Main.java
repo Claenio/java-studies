9313c884c901d77908db170412ce860969941b61
Claenio <claenio.souza@gmail.com>
2026-03-18 21:45:33 -0300

    small adjustments

--- CS50-exercises/population-growth/src/Main.java
+++ CS50-exercises/population-growth/src/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Population population = new Population();
         Scanner scan = new Scanner(System.in);
-        //VERIFICAR SE A POPULAÇÃO INICIAL É NO MÍNIMO 9
-        //O TAMANHO FINAL DEVE SER NO MÍNIMO O TAMANHO INICIAL
-        //CALCULAR QUANTOS ANOS VAI PRECISAR PARA ATINGIR A QUANTIDADE FINAL
 
         do {
             System.out.print("Enter population start size: ");

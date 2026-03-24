6ce30c55dfc4362b6c3ca060b447ec2bbdcf1bf4
Claenio <claenio.souza@gmail.com>
2026-03-23 21:03:26 -0300

    started scrabble pSet

---
+++ CS50-exercises/scrabble/src/Scrabble.java
@@ -0,0 +1,8 @@
+public class Scrabble {
+
+    private int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    private String word = "java";
+    char[] wordArray = word.toCharArray();
+
+
+}

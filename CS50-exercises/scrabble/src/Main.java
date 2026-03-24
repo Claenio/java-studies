063eec4941296d36541aeadced7c879c1099cb53
Claenio <claenio.souza@gmail.com>
2026-03-23 23:24:03 -0300

    scrabble in progress

--- CS50-exercises/scrabble/src/Main.java
+++ CS50-exercises/scrabble/src/Main.java
@@ -1,15 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-        String word = "java";
-        char[] wordArray = word.toCharArray();
-        int sum = 0;
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < wordArray.length; i++) {
-            int wordArrayIndex = wordArray[i] - 'a';
-            sum += points[wordArrayIndex];
-        }
-        System.out.println(sum);
+
 
     }
 }
--- CS50-exercises/scrabble/src/Scrabble.java
+++ CS50-exercises/scrabble/src/Scrabble.java
@@ -1,8 +1,23 @@
 public class Scrabble {
 
-    private int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-    private String word = "java";
-    char[] wordArray = word.toCharArray();
+    int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    String input = "Java?";
+    String word = input.toLowerCase();
+    private char[] wordArray = word.toCharArray();
+    int sum = 0;
+
+    public int calculate() {
+        for (int i = 0; i <= wordArray.length; i++) {
+            int wordArrayIndex = wordArray[i] - 'a';
+            if (wordArrayIndex >= 26 || wordArrayIndex < 0) {
+                wordArrayIndex = 0;
+            } else {
+                sum += points[wordArrayIndex];
+            }
+        }
+        return sum;
+    }
+
 
 
 }

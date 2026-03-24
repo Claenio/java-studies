422df0a9c25f6ca5b5f52a8d355a675812b20e7a
Claenio <claenio.souza@gmail.com>
2026-03-24 19:58:37 -0300

    solved scrabble problem

--- CS50-exercises/scrabble/src/Main.java
+++ CS50-exercises/scrabble/src/Main.java
@@ -4,7 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Scrabble player1 = new Scrabble();
+        Scrabble player2 = new Scrabble();
 
+        System.out.print("Player 1: ");
+        player1.setInput(scan.nextLine());
+        System.out.print("Player 2: ");
+        player2.setInput(scan.nextLine());
+        String[] word = {player1.getInput(), player2.getInput()};
+        int[] score = {player1.calculate(), player2.calculate()};
+
+        if (score[0] > score[1]){
+            System.out.println("\nPlayer 1 wins!");
+        } else if (score[0] < score[1]){
+            System.out.println("\nPlayer 2 wins!");
+        } else {
+            System.out.println("\nTie!");
+        }
+
+        System.out.printf("\nPlayer 1 word: %s | Points: %d!\nPlayer 2 word: %s | Points: %d!", word[0], score[0], word[1], score[1]);
+        scan.close();
 
     }
 }
--- CS50-exercises/scrabble/src/Scrabble.java
+++ CS50-exercises/scrabble/src/Scrabble.java
@@ -1,13 +1,21 @@
 public class Scrabble {
+    private int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    private String input;
+    private String word;
+    private char[] wordArray;
 
-    int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-    String input = "Java?";
-    String word = input.toLowerCase();
-    private char[] wordArray = word.toCharArray();
-    int sum = 0;
+    public void setInput(String input){
+        this.input = input;
+        this.word = input.toLowerCase();
+        wordArray = word.toCharArray();
+    }
+    public String getInput(){
+        return word;
+    }
 
     public int calculate() {
-        for (int i = 0; i <= wordArray.length; i++) {
+        int sum = 0;
+        for (int i = 0; i < wordArray.length; i++) {
             int wordArrayIndex = wordArray[i] - 'a';
             if (wordArrayIndex >= 26 || wordArrayIndex < 0) {
                 wordArrayIndex = 0;
@@ -17,7 +25,4 @@ public class Scrabble {
         }
         return sum;
     }
-
-
-
 }

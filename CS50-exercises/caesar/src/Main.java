20c9f1d847919b3f18b5280811a2185b5e70b512
Claenio <claenio.souza@gmail.com>
2026-03-29 23:19:27 -0300

    finished caesar problem

--- CS50-exercises/caesar/src/Main.java
+++ CS50-exercises/caesar/src/Main.java
@@ -1,8 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Caesar caesar = new Caesar();
-        caesar.encrypt("Hello", 2);
+        Scanner scan = new Scanner(System.in);
+        boolean menuLoop = true;
+        int key = 0;
 
+        while (menuLoop){
+            int option = 0;
+            String text = "";
+            System.out.println("""
+                    1 - Encrypt a text
+                    2 - Decrypt the text
+                    3 - quit
+                    """);
+            System.out.print("Select: ");
+            option = scan.nextInt();
+            scan.nextLine();
 
+            switch (option){
+                case 1:
+                    System.out.print("Enter a text: ");
+                    text = scan.nextLine();
+                    System.out.print("Enter a key: ");
+                    key = scan.nextInt();
+                    String encrypted = caesar.encrypt(text, key);
+                    System.out.println("\n" + "Encrypted text: " + encrypted + "\n");
+                    break;
+
+                case 2:
+                    String textToDecrypt = "";
+                    System.out.print("Text: ");
+                    textToDecrypt = scan.nextLine();
+                    System.out.print("Key: ");
+                    key = scan.nextInt();
+                    System.out.println("\n" + "Decrypted text: " + caesar.decrypt(textToDecrypt, key) + "\n");
+                     break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    menuLoop = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option");
+
+            }
+        }
+        scan.close();
     }
 }

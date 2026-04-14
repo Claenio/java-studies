87d89d68fbedc3bad0c4d6d4b396fa9a3056f0f4
Claenio <claenio.souza@gmail.com>
2026-04-14 20:22:41 -0300

    Selection sort algorithm

--- algorithms/algorithms.md
+++ algorithms/algorithms.md
@@ -53,3 +53,6 @@ for (int i = 0; i < array.length; i++) {
 }
 System.out.println(Arrays.toString(array));
 ```
+
+* ### Selection sort
+* 
---
+++ algorithms/src/src/SelectionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args){
+        int[] array = {7, 2, 5, 3, 1, 0};
+
+        for(int i = 0; i < array.length; i++){
+            int minNumberIndex = i;
+
+            for(int j = i+1; j < array.length; j++){
+                if(array[j] < array[minNumberIndex]){
+                    minNumberIndex = j;
+                }
+            }
+
+            int tempNumber = array[i];
+            array[i] = array[minNumberIndex];
+            array[minNumberIndex] = tempNumber;
+            System.out.println(Arrays.toString(array));
+        }
+
+    }
+}
